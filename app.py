3ca381cc602ac7fc5f98f7138a09ccb3dc26baed
Nanda Kishore <98215088+NandaKishoreYadav@users.noreply.github.com>
2024-07-23 11:19:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -41,4 +41,4 @@ def process_frame():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0',debug=True,ssl_context='adhoc')
